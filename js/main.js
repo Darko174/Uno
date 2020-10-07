f4b4745064b6fe9da06735845982559d7add6d59
Blayz74 <Slioo1691@gmail.com>
2020-10-07 19:01:41 +0500

    _

--- js/main.js
+++ js/main.js
@@ -3,42 +3,27 @@ const playerCards = document.querySelector(".playerCards");
 const enemyCards = document.querySelector(".enemyCards");
 const actionArea = document.querySelector(".actionArea");
 
-const gameInfo = {
-    firstPlayer: {
-        gameOver: false,
-        quantityMoves: 1,
-        active: true
-    },
-    secondPlayer:{
-        gameOver: false,
-        quantityMoves: 1,
-        active: true
-    }
-}
-
-class Game{
+class Game {
     //Создаем колоду
-    constructor(cards){
+    constructor(cards) {
         this.cards = this.shuffle(cards);
-        
+
     }
     //Перемешиваем колоду
-    shuffle(array){
-        for(let i = 0; i < 500; i++){
+    shuffle(array) {
+        for (let i = 0; i < 500; i++) {
             array.sort(() => Math.random() - 0.5);
         }
         return array
     }
     //Рендерим карточку
-    createCard(target, cardInfo){
+    createCard(target, cardInfo) {
         const card = document.createElement("div");
         card.classList.add("card");
         card.number = cardInfo.number;
         card.letter = cardInfo.letter;
         card.t = cardInfo.text;
-        /* target == playerCards ? card.style.backgroundImage = `url(${cardInfo.imgUrl})` : card.style.background = "linear-gradient(#c6e4ee 0%, #c6e4ee 40%, #fed1ae 60%, #faa0b9 70%, #cb7dcb 80%, #757ecb 100%)";  */
-        //card.style.backgroundImage = `url(${cardInfo.imgUrl})`;
-        if(target == playerCards) {
+        if (target == playerCards) {
             card.style.backgroundImage = `url(${cardInfo.imgUrl})`;
         }
         else {
@@ -51,19 +36,19 @@ class Game{
         array.forEach(i => this.createCard(target, i))
     }
 }
-class Player{
+class Player {
     //Создаем игрока
-    constructor(array){
+    constructor(array) {
         this.cards = this.getCards(array);
     }
     //Распределяем карты для игроков
-    getCards(array){                                                                   /////!!!!!
+    getCards(array) {                                                                   /////!!!!!
         const playerCards = [];
         const enemyCards = [];
-        for(let i = 0; i < 7; i++) {
-            console.log(playerCards.push(array.pop()), "Первому" );
-            
-            console.log(enemyCards.push(array.pop()), "Второму" );
+        for (let i = 0; i < 7; i++) {
+            console.log(playerCards.push(array.pop()), "Первому");
+
+            console.log(enemyCards.push(array.pop()), "Второму");
         }
         return {
             firstPlayer: playerCards,
@@ -93,44 +78,48 @@ function addCard(player, deck) {
 //Страрт игры
 function init() {
     buttonStartGame.classList.toggle("hide");
-
 }
 function gameOver() {
     playerCards.childNodes.length ? true : alert("You WIN! =)");
-    enemyCards.childNodes.length ? true : alert("You Lose! =(");
+    
 }
 //ИИ
 function artificialIntelligence() {
     const aiCards = Array.from(enemyCards.childNodes);
     const cardInTable = actionArea.querySelector(".card");
-    const sortedCardsByNumber = aiCards.map(i => i).sort((a,b) => a.number - b.number);
+    const sortedCardsByNumber = aiCards.map(i => i).sort((a, b) => a.number - b.number);
     let noCard = true;
+    if (playerCards.childNodes.length) {
+        for (let i of sortedCardsByNumber) {
+            if (i.number == cardInTable.number || i.letter == cardInTable.letter) {
+                setTimeout(() => {
+                    i.style.background = "";
+                    i.style.backgroundImage = i.bottomImage;
+                    i.classList.add("a");
+                    actionArea.innerHTML = "";
+                    actionArea.appendChild(i);
 
-    for(let i of sortedCardsByNumber) {
-        if(i.number == cardInTable.number || i.letter == cardInTable.letter) {
-            setTimeout(() => {
-                i.style.background = "";
-                i.style.backgroundImage = i.bottomImage;
-                i.classList.add("a");
-                actionArea.innerHTML = "";
-                actionArea.appendChild(i);
-            }, 2000);
-            noCard = false;
-            break
+                }, 2000);
+                noCard = false;
+                break
+            }
+        }
+        if (noCard) {
+            addCard(secondPlayer, newGame.cards);
+            newGame.createCard(enemyCards, secondPlayer[secondPlayer.length - 1]);
         }
     }
-    if(noCard) {
-        addCard(secondPlayer, newGame.cards);
-        newGame.createCard(enemyCards, secondPlayer[secondPlayer.length - 1]); 
-    }
+    setTimeout(() => {
+        enemyCards.childNodes.length ? true : alert("You Lose! =(");
+    },2500)
 }
 document.addEventListener("click", e => {
     const target = e.target;
     const cardInTable = actionArea.querySelector(".card");
 
     //Кладем карту на стол
-    if(target.closest(".card")) {
-        if(!cardInTable || target.number == cardInTable.number || target.letter == cardInTable.letter) {
+    if (target.closest(".card")) {
+        if (!cardInTable || target.number == cardInTable.number || target.letter == cardInTable.letter) {
             action(target);
             ////
             newGame.shuffle(newGame.cards);
@@ -140,14 +129,14 @@ document.addEventListener("click", e => {
             gameOver();
         }
     }
-    if(target.closest(".cardsStorage")) {
-            addCard(firstPlayer,newGame.cards);
-            newGame.createCard(playerCards, firstPlayer[firstPlayer.length - 1]);
-            ////
-            newGame.shuffle(newGame.cards);
-            console.log(newGame.cards);
-            ////
-            artificialIntelligence();
+    if (target.closest(".cardsStorage")) {
+        addCard(firstPlayer, newGame.cards);
+        newGame.createCard(playerCards, firstPlayer[firstPlayer.length - 1]);
+        ////
+        newGame.shuffle(newGame.cards);
+        console.log(newGame.cards);
+        ////
+        artificialIntelligence();
     }
 })
 buttonStartGame.addEventListener("click", init);
